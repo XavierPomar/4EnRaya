ae8f7d592a7db0d16844fa3e875ea9d88863ba24
XavierPomar <xaviier2922@gmail.com>
2020-06-05 19:50:50 +0200

    Commit de Jugador del 4 en raya

---
+++ src/pkg4_en_raya/Jugador.java
@@ -0,0 +1,20 @@
+package pkg4_en_raya;
+
+public class Jugador {
+    private String nom;
+    
+    //Constructor
+    public Jugador(){
+
+    }
+    
+    //Set & Get
+    
+    public void setnom(String nom){
+        this.nom = nom;
+    }
+    
+    public String getnom(){
+        return nom;
+    }
+}
